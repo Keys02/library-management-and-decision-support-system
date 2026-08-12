692b90de1da4f542a4cac3f1a0073a463c69c645
Keys <keys14052002@gmail.com>
2026-08-12 09:39:46 +0000

    feat: define the resource model

---
+++ src/model/Resource.java
@@ -0,0 +1,85 @@
+package model;
+
+public class Resource {
+    private int id;
+    private String name;
+    private String type;
+    private double cost;
+    private int quantity;
+    private int value;
+    private int libraryId;
+
+    public Resource(
+            int id,
+            String name,
+            String type,
+            double cost,
+            int quantity,
+            int value,
+            int libraryId
+    ) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.cost = cost;
+        this.quantity = quantity;
+        this.value = value;
+        this.libraryId = libraryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getLibraryId() {
+        return libraryId;
+    }
+
+    public void setLibraryId(int libraryId) {
+        this.libraryId = libraryId;
+    }
+}
