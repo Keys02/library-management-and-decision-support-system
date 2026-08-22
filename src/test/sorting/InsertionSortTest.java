c8a3ebffeab7a4879d7f9fda205eea6956abbeea
AnsuBadu <vbansu@st.ug.edu.gh>
2026-08-22 08:33:19 +0000

    feat: implement insertion sort algorithm

---
+++ src/test/sorting/InsertionSortTest.java
@@ -0,0 +1,31 @@
+package test.sorting;
+
+import algorithms.sorting.InsertionSort;
+
+public class InsertionSortTest {
+
+    public static void main(String[] args) {
+
+        Integer[] numbers = {
+
+                68,
+                25,
+                42,
+                8,
+                12
+
+        };
+
+        InsertionSort.sort(numbers);
+
+        System.out.println("Sorted Array");
+
+        for (Integer number : numbers) {
+
+            System.out.print(number + " ");
+
+        }
+
+    }
+
+}
