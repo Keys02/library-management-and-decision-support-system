9baa648270e15c86e564145db07a495f7d1e7f33
AnsuBadu <vbansu@st.ug.edu.gh>
2026-08-22 21:38:51 +0000

    feat: complete red-black tree insertion and balancing

--- src/test/tree/RedBlackTreeTest.java
+++ src/test/tree/RedBlackTreeTest.java
@@ -7,24 +7,15 @@ public class RedBlackTreeTest {
     public static void main(String[] args) {
 
         BinaryRedBlackTree<Integer> tree =
-                new BinaryRedBlackTree<>();
+        new BinaryRedBlackTree<>();
 
-        tree.insert(10);
         tree.insert(20);
+        tree.insert(10);
         tree.insert(30);
-        tree.insert(15);
-        tree.insert(25);
         tree.insert(5);
 
         System.out.println("Root: " + tree.getRoot().getData());
         System.out.println("Root is red? " + tree.getRoot().isRed());
-
-        System.out.println("Root: "
-                + tree.getRoot().getData());
-
-        System.out.println("Root is red? "
-                + tree.getRoot().isRed());
-
     }
 
 }
